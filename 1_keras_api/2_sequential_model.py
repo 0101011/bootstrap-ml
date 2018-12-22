70b4bef6c3aee2557112b271e3da3561833f47ff
Andrew Stepin <andrew.stepin@gmail.com>
2018-12-22 21:23:49 +0200

    running the sample model on CPU

--- 1_keras_api/2_sequential_model.py
+++ 1_keras_api/2_sequential_model.py
@@ -1,7 +1,24 @@
+"""
+Running the tiny sample model is faster on the CPU: Batch loading 
+from RAM to GPU is slower at the start of each operation. Forward/backward 
+computations are very quick in tiny networks so it's rational to use CPU.
+You can also try using model.fit_generator instead of plain fit, so that 
+CPU thread which loads minibatches works in parallel. 
+
+At the time there is no way I am aware of to preload the whole dataset 
+on GPU with Keras.
+"""
+
+# Hiding a GPU
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+
+# Importing tf and numpy
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+# Defining and running the model
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
